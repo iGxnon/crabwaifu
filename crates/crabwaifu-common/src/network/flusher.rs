596e76b3057d8983b53c81940af23f3751135b45
iGxnon <igxnon@gmail.com>
2025-02-02 15:28:44 +0800

    new flusher
    Signed-off-by: iGxnon <igxnon@gmail.com>

--- crates/crabwaifu-common/src/network/flusher.rs
+++ crates/crabwaifu-common/src/network/flusher.rs
@@ -4,60 +4,41 @@ use std::time::Duration;
 
 use futures::SinkExt;
 use raknet_rs::Message;
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
-use tokio::sync::{mpsc, oneshot, Notify};
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::sync::{oneshot, Notify};
 use tokio::task::JoinHandle;
 use tokio::time;
 
 use super::PinWriter;
 use crate::utils::TimeoutWrapper;
 
-const DEFAULT_FLUSH_DELAY: Duration = Duration::from_millis(1); // The maximum delay accepted by peer or the minimum delay of processing a piece of data
-const DEFAULT_CLOSE_TIMEOUT: Duration = Duration::from_secs(5);
-
 /// Spawn the flush task and return the handles of it
 pub fn spawn_flush_task(
     writer: impl PinWriter,
 ) -> (
-    UnboundedSender<Message>,
+    Sender<Message>,
     Arc<Notify>,
     oneshot::Sender<bool>,
     JoinHandle<()>,
 ) {
-    let mut ticker = time::interval(DEFAULT_FLUSH_DELAY);
-    ticker.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
-
-    let (mut flusher, tx) = Flusher::new(writer, ticker);
-    let flush_notify = Arc::new(Notify::new());
+    let (mut flusher, flush_notify, tx) = Flusher::new(writer);
     let (close_tx, mut close_rx) = oneshot::channel();
-
-    let flush_notify_clone = flush_notify.clone();
     let handle = tokio::spawn(async move {
-        let wait_2msl = loop {
-            tokio::select! {
-                _ = flusher.wait() => {
-                    if let Err(err) = flusher.flush().await {
-                        log::error!("unexpected flush error {err}, terminating flush task");
-                        break false;
-                    }
-                }
-                _ = flush_notify_clone.notified() => {
-                    if let Err(err) = flusher.flush().await {
-                        log::error!("unexpected flush error {err}, terminating flush task");
-                        break false;
-                    }
-                }
-                res = &mut close_rx => break res.expect("channel never close"),
+        let wait_2msl = tokio::select! {
+            Ok(wait) = &mut close_rx => wait,
+            Err(err) = flusher.run() => {
+                log::error!("unexpected flush error {err}, terminating flush task");
+                false
             }
         };
 
-        log::info!("destroying flusher...");
+        log::info!("closing flusher...");
         if let Err(err) = flusher.close().await {
             log::error!("unexpected closing error {err}");
             return;
         }
         log::info!("flusher closed");
-        
+
         if !wait_2msl {
             return;
         }
@@ -74,52 +55,57 @@ pub fn spawn_flush_task(
 /// A naive auto balanced flush controller for each connection
 struct Flusher<W: PinWriter> {
     writer: W,
-    buffer: UnboundedReceiver<Message>,
     ticker: time::Interval,
+    notifier: Arc<Notify>,
+    rx: Receiver<Message>,
 }
 
 impl<W: PinWriter> Flusher<W> {
-    fn new(writer: W, ticker: time::Interval) -> (Self, UnboundedSender<Message>) {
-        let (tx, rx) = mpsc::unbounded_channel();
-        let flusher = Self {
+    // The maximum delay accepted by peer or the minimum delay of processing a piece of data
+    const DEFAULT_CLOSE_TIMEOUT: Duration = Duration::from_secs(5);
+    const DEFAULT_FLUSH_DELAY: Duration = Duration::from_millis(1);
+    const MAX_FLUSH_PENDING: usize = 1;
+
+    fn new(writer: W) -> (Self, Arc<Notify>, Sender<Message>) {
+        let mut ticker = time::interval(Self::DEFAULT_FLUSH_DELAY);
+        ticker.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
+        let notifier = Arc::new(Notify::new());
+        let (tx, rx) = mpsc::channel(Self::MAX_FLUSH_PENDING);
+        let me = Self {
             writer,
-            buffer: rx,
             ticker,
+            notifier: notifier.clone(),
+            rx,
         };
-        (flusher, tx)
+        (me, notifier, tx)
     }
 
-    #[inline(always)]
-    async fn wait(&mut self) {
-        self.ticker.tick().await;
-    }
-
-    #[inline(always)]
-    async fn flush(&mut self) -> io::Result<()> {
-        // non-blocking recv
-        while let Ok(msg) = self.buffer.try_recv() {
-            self.writer.feed(msg).await?;
+    async fn run(&mut self) -> io::Result<()> {
+        loop {
+            tokio::select! {
+                _ = self.ticker.tick() => {
+                    self.writer.flush().await?;
+                }
+                _ = self.notifier.notified() => {
+                    self.writer.flush().await?;
+                }
+                Some(msg) = self.rx.recv() => {
+                    self.writer.feed(msg).await?;
+                }
+            }
         }
-        self.writer.flush().await?;
-        Ok(())
     }
 
     /// Close the writer with timeout
-    #[inline(always)]
     async fn close(&mut self) -> io::Result<()> {
-        self.writer.close().timeout(DEFAULT_CLOSE_TIMEOUT).await?
+        self.writer
+            .close()
+            .timeout(Self::DEFAULT_CLOSE_TIMEOUT)
+            .await?
     }
 
-    #[inline(always)]
     async fn last_2msl(&mut self) -> io::Result<()> {
-        let task = async {
-            loop {
-                self.wait().await;
-                self.flush().await?;
-            }
-        }
-        .timeout(2 * DEFAULT_CLOSE_TIMEOUT);
-        match task.await {
+        match self.run().timeout(2 * Self::DEFAULT_CLOSE_TIMEOUT).await {
             Ok(res) => res,
             Err(_) => Ok(()),
         }
